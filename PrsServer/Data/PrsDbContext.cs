66cc50d60f7f8adb82dafd9c34cb5286a5fb374c
Dustin Fox <dustinfox1996@gmail.com>
2021-03-08 10:53:26 -0500

    first edit

---
+++ PrsServer/Data/PrsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrsServer.Models;
+
+namespace PrsServer.Data
+{
+    public class PrsDbContext : DbContext
+    {
+        public PrsDbContext (DbContextOptions<PrsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<User>(e => { })
+        }
+
+    }
+}
