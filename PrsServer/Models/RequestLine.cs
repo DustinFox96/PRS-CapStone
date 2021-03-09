11d3d2dee5a7158966f724c71b62b4a0bf3d5fc1
Dustin Fox <djf149@hotmail.com>
2021-03-09 11:50:08 -0500

    Made updates to the RequestLine model and controller

--- PrsServer/Models/RequestLine.cs
+++ PrsServer/Models/RequestLine.cs
@@ -11,6 +11,7 @@ namespace PrsServer.Models
         public int RequestId { get; set; }
         public virtual Request Request { get; set; }
         public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; } = 1;
     }
 }
