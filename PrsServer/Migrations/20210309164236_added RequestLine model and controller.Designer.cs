40ba0619ae8b8cd81f2da729bd5bf6aeb822293c
Dustin Fox <djf149@hotmail.com>
2021-03-09 11:45:20 -0500

    added the migration and updated database with Orderlines stuff

--- PrsServer/Migrations/20210309164236_added RequestLine model and controller.Designer.cs
+++ PrsServer/Migrations/20210309164236_added RequestLine model and controller.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrsServer.Data;
 
 namespace PrsServer.Migrations
 {
     [DbContext(typeof(PrsDbContext))]
-    partial class PrsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210309164236_added RequestLine model and controller")]
+    partial class addedRequestLinemodelandcontroller
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
